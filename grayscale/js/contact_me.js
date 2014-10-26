82178a470e734b7325593c7371e325fd4b989ce5
Bri <briannelmurphy@gmail.com>
2014-10-26 23:35:17 +0000

    submit button

--- grayscale/js/contact_me.js
+++ grayscale/js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
 	   firstName = name.split(' ').slice(0, -1).join(' ');
          }        
 	 $.ajax({
-                url: "./bin/contact_me.php",
+                url: "php/emailContact.php",
             	type: "POST",
             	data: {name: name, email: email, message: message},
             	cache: false,
@@ -34,17 +34,17 @@ $(function() {
  			.append('</div>');
  						    
  		  //clear all fields
- 		  $('#contactForm').trigger("reset");
+ 		  $('#contact-form').trigger("reset");
  	      },
  	   error: function() {		
  		// Fail message
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:guy.shrubsole@foe.co.uk?Subject=Run On Sun Website Contact'>guy.shrubsole@foe.co.uk</a> ? Sorry for the inconvenience!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
- 		$('#contactForm').trigger("reset");
+ 		$('#contact-form').trigger("reset");
  	    },
            })
          },
